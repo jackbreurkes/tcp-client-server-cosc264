c6e2ad2de605e11b66791ad8947d937876b54114
Jack Breurkes <jack.breurkes@gmail.com>
2019-08-04 15:58:11 +1200

    updated client and server to point where data is being written to client files (not quite in the correct way maybe?)

--- client.py
+++ client.py
@@ -1,18 +1,118 @@
 #!/usr/local/bin/python3
 
+import os
 import sys
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 10234)
-try:
-    sock.connect(server_address)
-except socket.timeout as e:
-    print(e)
-except OSError as e:
-    print(e)
-except ConnectionRefusedError as e:
-    print(e)
 
-message = 'testing the messaging capabilities of my TCP client server combo'
-sock.send(message.encode('utf-8'))
+def main():
+    if len(sys.argv) == 1:
+        # DEBUG
+        args = ['', 'localhost', '10234', 'test_file.txt']
+    else:
+        args = sys.argv
+
+    if len(args) != 4:
+        sys.exit('usage: client.py [sever ip] [files port number] [file name]')
+
+    try:
+        addr = socket.getaddrinfo(args[1], args[2])
+    except socket.gaierror:
+        sys.exit('error getting address info.')
+
+    try:
+        portnum = int(args[2])
+    except ValueError:
+        sys.exit('invalid port number argument.')
+
+    if not (1024 <= portnum <= 64000):
+        sys.exit('port number must be between 1024 and 64000 (inclusive).')
+
+    # if os.path.exists(args[3]) and os.path.isfile(args[3]):
+    #     sys.exit('a local file named {} already exists. please rename it or choose a different file.'.format(args[3]))
+    # ^^ DEBUG
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except OSError:
+        sys.exit('could not create a socket.')
+
+    valids = [tup for tup in addr if tup[0] == socket.AddressFamily.AF_INET and tup[1] == socket.SocketKind.SOCK_STREAM]
+    if len(valids) == 0:
+        sock.close()
+        sys.exit("the service {} at port {} does not support stream sockets via IPv4.")
+    try:
+        sock.connect(valids[0][4])
+    except OSError as e:
+        sock.close()
+        sys.exit("could not connect to the file server.")
+
+    message = createfilerequest(0x497E, 1, args[3])
+    sock.send(message)
+
+    readfileresponse(sock, args[3])
+
+
+def createfilerequest(magic_num, type_num, filename):
+    filename_bytes = filename.encode('utf-8')
+
+    filename_bitlen = len(filename_bytes) * 8
+    binary = int.from_bytes(filename_bytes, 'big')
+    binary |= len(filename_bytes) << filename_bitlen
+    binary |= type_num << (filename_bitlen + 16)
+    binary |= magic_num << (filename_bitlen + 16 + 8)
+    pkt = int.to_bytes(binary, 5 + len(filename_bytes), 'big')
+    return pkt
+
+
+def readfileresponse(conn, filename):
+    try:
+        pktheader = conn.recv(8)
+    except socket.timeout:
+        print('timeout reading FileResponse header')
+        conn.close()
+        return
+
+    if int.from_bytes(pktheader[0:2], 'big') != 0x497E:
+        print('FileResponse has invalid MagicNo field value.')
+        conn.close()
+        return
+    if pktheader[2] != 2:
+        print('FileResponse has invalid Type field value.')
+        conn.close()
+        return
+
+    if pktheader[3] not in [0, 1]:
+        print('FileResponse has invalid StatusCode field value.')
+        conn.close()
+        return
+    if pktheader[3] == 0:
+        print('Requested file could not be retrieved by the server.')
+        conn.close()
+        return
+
+    try:
+        f = open(filename, 'w+')
+    except IOError:
+        conn.close()
+        sys.exit('could not open {} locally.'.format(filename))
+
+    filesize = int.from_bytes(pktheader[4:8], 'big')
+    while filesize > 0:
+        try:
+            filedata = conn.recv(min(filesize, 4096))
+        except socket.timeout:
+            print('timeout while reading FileResponse body')
+            conn.close()
+            return
+
+        try:
+            f.write(filedata.decode('utf-8'))
+        except IOError:
+            conn.close()
+            sys.exit('error while writing received data to local file.')
+        filesize -= 4096
+
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -2,28 +2,10 @@
 
 import sys
 import socket
+from datetime import datetime
 
 MIN_PORT = 1024
 MAX_PORT = 64000
-
-
-def get_port_from_args(args):
-    if len(args) != 2:
-        print("usage: server.py.py [port number]")
-        sys.exit(1)
-    port_str = args[1]
-
-    try:
-        port_number = int(port_str)
-    except ValueError as e:
-        print("please enter an integer as a port number")
-        sys.exit(2)
-
-    if port_number not in range(MIN_PORT, MAX_PORT + 1):
-        print("please enter a port number between {} and {}".format(MIN_PORT, MAX_PORT))
-        sys.exit(3)
-
-    return port_number
 
 
 def main():
@@ -34,24 +16,104 @@ def main():
         args = sys.argv
 
     port = get_port_from_args(args)
+
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except OSError:
-        print("Error creating socket:", e)
-        sys.exit(4)
-        
+        sys.exit("Error creating socket.")
+
     try:
         sock.bind(('localhost', port))
-    except OSError as e:
-        print(e)
-        sys.exit(5)
+    except OSError:
+        sys.exit("Error binding socket to given port.")
 
-    sock.listen()
-    connection, client_addr = sock.accept()
-    data = sock.recv(200)
-    print(data.decode())
-    print("conn & done")
-    sock.close()
+    try:
+        sock.listen()
+    except OSError as e:
+        sock.close()
+        sys.exit("Could not set the socket to accept incoming connections.")
+
+    while True:
+        print('')
+        connection, clientaddrinfo = sock.accept()
+        connection.settimeout(1)
+        print("connection accepted from", clientaddrinfo[0], "on port", clientaddrinfo[1], "at", datetime.now())
+        readfilerequest(connection)
+
+
+def get_port_from_args(args):
+    if len(args) != 2:
+        sys.exit("usage: files.py [port number]")
+    port_str = args[1]
+
+    try:
+        port_number = int(port_str)
+    except ValueError as e:
+        sys.exit("please enter an integer as a port number")
+
+    if port_number not in range(MIN_PORT, MAX_PORT + 1):
+        sys.exit("please enter a port number between {} and {}".format(MIN_PORT, MAX_PORT))
+
+    return port_number
+
+
+def readfilerequest(conn):
+    try:
+        pktheader = conn.recv(5)
+    except socket.timeout:
+        print('timeout reading FileRequest header')
+        conn.close()
+        return
+
+    if int.from_bytes(pktheader[0:2], 'big') != 0x497E:
+        print('FileRequest has invalid MagicNo field value.')
+        conn.close()
+        return
+    if pktheader[2] != 1:
+        print('FileRequest has invalid Type field value.')
+        conn.close()
+        return
+    filenamelen = int.from_bytes(pktheader[3:5], 'big')
+    if not (1 <= filenamelen <= 1024):
+        print('FileRequest has invalid FilenameLen field value.')
+        conn.close()
+        return
+
+    try:
+        filename_bytes = conn.recv(filenamelen)
+    except socket.timeout:
+        print('timeout reading FileRequest Filename field value.')
+        conn.close()
+        return
+
+    filename = "server_files/" + filename_bytes.decode('utf-8')
+    try:
+        f = open(filename)
+    except IOError:
+        res = createfileresponse(0x497E, 2, 0)
+        conn.send(res)
+        conn.close()
+        print('{} does not exist or could not be opened.'.format(filename))
+        return
+
+    filedata = f.read().encode('utf-8')
+    f.close()
+    res = createfileresponse(0x497E, 2, 1, filedata)
+    conn.send(res)
+    conn.close()
+    print('transfer of file {} completed. {} bytes transferred in total.'.format(filename, len(res)))
+
+
+def createfileresponse(magic_num, type_num, status_code, filedata=bytes()):
+    filedata_bitlen = len(filedata) * 8
+
+    binary = int.from_bytes(filedata, 'big')
+    binary |= len(filedata) << filedata_bitlen
+    binary |= status_code << (filedata_bitlen + 32)
+    binary |= type_num << (filedata_bitlen + 32 + 8)
+    binary |= magic_num << (filedata_bitlen + 32 + 8 + 8)
+    pkt = int.to_bytes(binary, 8 + len(filedata), 'big')
+    return pkt
 
 
 if __name__ == '__main__':
